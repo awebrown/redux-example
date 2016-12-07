a2c01b14d50e553bc0781cd02ee8d23bb1f33a56
Anthony Brown <awebrown@gmail.com>
2016-12-07 18:53:35 -0500

    Add test template

---
+++ app/tests/components/Template.test.jsx
@@ -0,0 +1,12 @@
+let React = require('react'),
+    ReactDom = require('react-dom'),
+    expect = require('expect'),
+    $ = require('jQuery'),
+    TestUtils = require('react-addons-test-utils'),
+    ComponentName = require('ComponentName');
+
+    describe('ComponentName', () => {
+      it('should exist', () => {
+        expect(TodoList).toExist();
+      });
+    });
