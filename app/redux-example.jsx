3192832f1be425106966ab93f88d0dddc063a900
Anthony Brown <awebrown@gmail.com>
2016-12-11 11:09:58 -0500

    Break up reducer into multiple reducers

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -14,53 +14,56 @@ let stateDefault = {
 let nextHobbyId = 1;
 let newMovieId = 1;
 
-let reducer = (state = stateDefault, action) => {
-  // state = state || {name: 'Anonymous'};
+let nameReducer = (state = 'Anonymous', action) => {
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return action.name;
+      break;
+    default:
+      return state;
+  }
+}
+
+let hobbiesReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_HOBBY':
+      return [
+        ...state,
+        {
+          id: nextHobbyId++,
+          hobby: action.hobby
+        }
+      ];
+      case 'REMOVE_HOBBY' :
+        return state.filter((hobby) => hobby.id !== action.id)
+      default:
+        return state;
+  }
+}
+
+let moviesReducer = (state = [], action) => {
   switch(action.type) {
-    case 'CHANGE_NAME' :
-      return {
-        ...state,
-        name: action.name
-      };
-    case 'ADD_HOBBY' :
-      return {
-        ...state,
-      hobbies: [
-          ...state.hobbies,
-          {
-            id: nextHobbyId++,
-            hobby: action.hobby
-           }
-        ]
-      };
-    case 'REMOVE_HOBBY' :
-        return {
-          ...state,
-          hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
-        };
     case 'ADD_MOVIE' :
-      return {
+      return [
         ...state,
-        movies: [
-          ...state.movies,
           {
             id: newMovieId++,
             title: action.title,
             genre: action.genre
           }
         ]
-      };
-      case 'REMOVE_MOVIE' :
-          return {
-            ...state,
-            movies: state.movies.filter((movie) => movie.id !== action.id)
-          };
+    case 'REMOVE_MOVIE' :
+      return state.filter((movie) => movie.id !== action.id)
     default:
       return state;
   }
-};
+}
 
-
+let reducer = redux.combineReducers({
+  name: nameReducer,
+  hobbies: hobbiesReducer,
+  movies: moviesReducer
+})
 
 let store = redux.createStore(reducer, redux.compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
